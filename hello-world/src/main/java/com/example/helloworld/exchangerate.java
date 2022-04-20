4b26c30ed67cd039eb402e651d53fba2e2881b34
XaviP99 <62841415+XaviP99@users.noreply.github.com>
2022-04-20 22:38:04 +0100

    Add files via upload

---
+++ hello-world/src/main/java/com/example/helloworld/exchangerate.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+public class exchangerate {
+
+    private String date;
+    private double   result;
+
+    public double getResult() {
+
+        return result;
+    }
+
+    public String getDate() {
+
+        return date;
+    }
+}
